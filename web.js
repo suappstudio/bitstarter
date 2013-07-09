478332f5063789e2ad9aee1bef68d1ef392b3995
Gaetano Lorusso <gaetanlor@gmail.com>
2013-07-09 17:07:05 +0000

    Correctons

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var buffer = new Buffer((fs.readFileSync('index.html'),'utf-8'));
+var buffer = new Buffer(fs.readFileSync('index.html','utf-8'));
 app.get('/', function(request, response) {
   response.send(buffer.toString);
 

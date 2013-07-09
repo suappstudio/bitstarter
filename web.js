368baea1fb257592de6695b66cb341160a143dee
Gaetano Lorusso <gaetanlor@gmail.com>
2013-07-09 17:48:13 +0000

    update

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 var buffer = new Buffer(fs.readFileSync('index.html'));
 app.get('/', function(request, response) {
-  response.send(buffer.toString);
+  response.send(buffer.toString());
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {

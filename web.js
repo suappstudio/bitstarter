a59608b83990d692d74943cbbaf94a87a367d93b
Gaetano Lorusso <gaetanlor@gmail.com>
2013-07-09 17:32:19 +0000

    commit

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 var buffer = fs.readFileSync('index.html','utf-8');
 app.get('/', function(request, response) {
   response.send(buffer.toString);
-
+}
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);

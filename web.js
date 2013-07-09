5ab0c3f406bc8f341571f05f771a69ed9b3c91c7
Gaetano Lorusso <gaetanlor@gmail.com>
2013-07-09 16:23:34 +0000

    Update first exercise try

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var buffer = (fs.readFile('index.html'),'utf8');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.send(buffer.toString);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
